3a276a0c24eaf5bf6c1107dad4c923746a56a535
Karl Kaether <karl@k70.bioinf.uni-leipzig.de>
2025-08-19 19:10:10 +0200

    update

--- scaffolder/get_weights.py
+++ scaffolder/get_weights.py
@@ -15,6 +15,7 @@ with open('refs.txt','r') as f:
 target = argv[1]
 
 weights = {}
+max_w = 0
 for ref in refs:
     aligned_len = 0
     with open(f'{anchors_path}/aligned/{ref}','rb') as f:
@@ -27,8 +28,16 @@ for ref in refs:
                 for j,bib_m in d['matches'][target]['matches'].items():
                     aligned_len += bib_m[f'hit coordinates in {target} candidate'][1] - bib_m[f'hit coordinates in {target} candidate'][0]
     weights[ref] = aligned_len
+    if aligned_len > max_w:
+        max_w = aligned_len
 
+factor = 1
+while max_w > 1e5:
+    for ref in weights:
+        weights[ref] = int(aligned_len/10)
+        if weights[ref] < 1:
+            weights[ref] = 1
 
 with open('ref_weights.txt','w') as f:
     for ref,aligned_len in weights.items():
-        f.write(f'{ref}\t{aligned_len}\n')
+        f.write(f'{ref}\t{int(aligned_len/factor)}\n')
